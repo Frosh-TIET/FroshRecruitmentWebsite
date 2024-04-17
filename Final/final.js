ad204d06dcdf558048c97ec5fc511a47416b46a0
navansh03 <navanshgoswami5@gmail.com>
2024-04-17 23:16:49 +0530

    mobile ke liye banadi

--- Final/final.js
+++ Final/final.js
@@ -1,26 +1,26 @@
-import { Application } from '@splinetool/runtime';
+// import { Application } from '@splinetool/runtime';
 
-const canvas = document.getElementById('canvas3d');
-if ("IntersectionObserver" in window) {
-  let app;
-  let observer = new IntersectionObserver(function(entries, observer) {
-    entries.forEach(function(entry) {
-      if (entry.isIntersecting) {
-        if (!app) {
-          app = new Application(canvas);
-          app.load('https://prod.spline.design/VufIgZe7y0IUh0fl/scene.splinecode');
-        }
-      } else {
-        if (app) {
-          let ctx = canvas.getContext('2d');
-          ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-          app = null; // Remove reference to the app
-        }
-      }
-    });
-  });
-  observer.observe(canvas);
-}
+// const canvas = document.getElementById('canvas3d');
+// if ("IntersectionObserver" in window) {
+//   let app;
+//   let observer = new IntersectionObserver(function(entries, observer) {
+//     entries.forEach(function(entry) {
+//       if (entry.isIntersecting) {
+//         if (!app) {
+//           app = new Application(canvas);
+//           app.load('https://prod.spline.design/VufIgZe7y0IUh0fl/scene.splinecode');
+//         }
+//       } else {
+//         if (app) {
+//           let ctx = canvas.getContext('2d');
+//           ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+//           app = null; // Remove reference to the app
+//         }
+//       }
+//     });
+//   });
+//   observer.observe(canvas);
+// }
 //arrow animatioin
 document.addEventListener("DOMContentLoaded", function () {
     const arrow = document.getElementById("scroll-down-arrow");
@@ -44,3 +44,14 @@ window.addEventListener('scroll', function() {
     document.querySelector('.bg').classList.add('wiggle');
   }, 150);
 }, false);
+
+
+// var video = document.getElementById('myVideo');
+
+// if (window.matchMedia('(max-width: 480)').matches) {
+//   // If the viewport is 600px or smaller, use small.mp4
+//   video.src = './Final/images/sohum.mp4';
+// } else {
+//   // Otherwise, use large.mp4
+//   video.src = './Final/images/navansh.mp4';
+// }
